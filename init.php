fd8f82638405619e0250a73f65d54eb263b9ce5e
Marc Jauvin <marc.jauvin@gmail.com>
2021-03-15 10:07:10 -0400

    prevent creating the class aliases when the original plugin is still installed

--- init.php
+++ init.php
@@ -1,23 +1,24 @@
 <?php
 
-class_alias(Winter\Blog\Plugin::class, RainLab\Blog\Plugin::class);
-class_alias(Winter\Blog\Controllers\Categories::class, RainLab\Blog\Controllers\Categories::class);
-class_alias(Winter\Blog\Controllers\Posts::class, RainLab\Blog\Controllers\Posts::class);
-class_alias(Winter\Blog\Updates\CategoriesAddNestedFields::class, RainLab\Blog\Updates\CategoriesAddNestedFields::class);
-class_alias(Winter\Blog\Updates\UpdateTimestampsNullable::class, RainLab\Blog\Updates\UpdateTimestampsNullable::class);
-class_alias(Winter\Blog\Updates\CreatePostsTable::class, RainLab\Blog\Updates\CreatePostsTable::class);
-class_alias(Winter\Blog\Updates\SeedAllTables::class, RainLab\Blog\Updates\SeedAllTables::class);
-class_alias(Winter\Blog\Updates\CreateCategoriesTable::class, RainLab\Blog\Updates\CreateCategoriesTable::class);
-class_alias(Winter\Blog\Updates\PostsAddMetadata::class, RainLab\Blog\Updates\PostsAddMetadata::class);
-class_alias(Winter\Blog\FormWidgets\BlogMarkdown::class, RainLab\Blog\FormWidgets\BlogMarkdown::class);
-class_alias(Winter\Blog\FormWidgets\MLBlogMarkdown::class, RainLab\Blog\FormWidgets\MLBlogMarkdown::class);
-class_alias(Winter\Blog\Classes\TagProcessor::class, RainLab\Blog\Classes\TagProcessor::class);
-class_alias(Winter\Blog\Models\Settings::class, RainLab\Blog\Models\Settings::class);
-class_alias(Winter\Blog\Models\PostImport::class, RainLab\Blog\Models\PostImport::class);
-class_alias(Winter\Blog\Models\Post::class, RainLab\Blog\Models\Post::class);
-class_alias(Winter\Blog\Models\Category::class, RainLab\Blog\Models\Category::class);
-class_alias(Winter\Blog\Models\PostExport::class, RainLab\Blog\Models\PostExport::class);
-class_alias(Winter\Blog\Components\Categories::class, RainLab\Blog\Components\Categories::class);
-class_alias(Winter\Blog\Components\Posts::class, RainLab\Blog\Components\Posts::class);
-class_alias(Winter\Blog\Components\Post::class, RainLab\Blog\Components\Post::class);
-class_alias(Winter\Blog\Components\RssFeed::class, RainLab\Blog\Components\RssFeed::class);
+if (!class_exists(RainLab\Blog\Plugin::class)) {
+    class_alias(Winter\Blog\Plugin::class, RainLab\Blog\Plugin::class);
+
+    class_alias(Winter\Blog\Classes\TagProcessor::class, RainLab\Blog\Classes\TagProcessor::class);
+
+    class_alias(Winter\Blog\Components\Categories::class, RainLab\Blog\Components\Categories::class);
+    class_alias(Winter\Blog\Components\Posts::class, RainLab\Blog\Components\Posts::class);
+    class_alias(Winter\Blog\Components\Post::class, RainLab\Blog\Components\Post::class);
+    class_alias(Winter\Blog\Components\RssFeed::class, RainLab\Blog\Components\RssFeed::class);
+
+    class_alias(Winter\Blog\Controllers\Categories::class, RainLab\Blog\Controllers\Categories::class);
+    class_alias(Winter\Blog\Controllers\Posts::class, RainLab\Blog\Controllers\Posts::class);
+
+    class_alias(Winter\Blog\FormWidgets\BlogMarkdown::class, RainLab\Blog\FormWidgets\BlogMarkdown::class);
+    class_alias(Winter\Blog\FormWidgets\MLBlogMarkdown::class, RainLab\Blog\FormWidgets\MLBlogMarkdown::class);
+
+    class_alias(Winter\Blog\Models\Settings::class, RainLab\Blog\Models\Settings::class);
+    class_alias(Winter\Blog\Models\PostImport::class, RainLab\Blog\Models\PostImport::class);
+    class_alias(Winter\Blog\Models\Post::class, RainLab\Blog\Models\Post::class);
+    class_alias(Winter\Blog\Models\Category::class, RainLab\Blog\Models\Category::class);
+    class_alias(Winter\Blog\Models\PostExport::class, RainLab\Blog\Models\PostExport::class);
+}
