fdb458327e4b2675e34e7856b1efa4a11f970bb8
Ben Thomson <git@alfreido.com>
2021-03-16 23:51:10 +0800

    Clean up aliases

--- init.php
+++ init.php
@@ -1,24 +1,3 @@
 <?php
-
-if (!class_exists(RainLab\Blog\Plugin::class)) {
-    class_alias(Winter\Blog\Plugin::class, RainLab\Blog\Plugin::class);
-
-    class_alias(Winter\Blog\Classes\TagProcessor::class, RainLab\Blog\Classes\TagProcessor::class);
-
-    class_alias(Winter\Blog\Components\Categories::class, RainLab\Blog\Components\Categories::class);
-    class_alias(Winter\Blog\Components\Posts::class, RainLab\Blog\Components\Posts::class);
-    class_alias(Winter\Blog\Components\Post::class, RainLab\Blog\Components\Post::class);
-    class_alias(Winter\Blog\Components\RssFeed::class, RainLab\Blog\Components\RssFeed::class);
-
-    class_alias(Winter\Blog\Controllers\Categories::class, RainLab\Blog\Controllers\Categories::class);
-    class_alias(Winter\Blog\Controllers\Posts::class, RainLab\Blog\Controllers\Posts::class);
-
-    class_alias(Winter\Blog\FormWidgets\BlogMarkdown::class, RainLab\Blog\FormWidgets\BlogMarkdown::class);
-    class_alias(Winter\Blog\FormWidgets\MLBlogMarkdown::class, RainLab\Blog\FormWidgets\MLBlogMarkdown::class);
-
-    class_alias(Winter\Blog\Models\Settings::class, RainLab\Blog\Models\Settings::class);
-    class_alias(Winter\Blog\Models\PostImport::class, RainLab\Blog\Models\PostImport::class);
-    class_alias(Winter\Blog\Models\Post::class, RainLab\Blog\Models\Post::class);
-    class_alias(Winter\Blog\Models\Category::class, RainLab\Blog\Models\Category::class);
-    class_alias(Winter\Blog\Models\PostExport::class, RainLab\Blog\Models\PostExport::class);
-}
+// Include aliases
+require_once __DIR__ . '/aliases.php';
