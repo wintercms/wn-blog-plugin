c0c952e2811c22f4acddf8ab711e2d33f4710d30
Marc Jauvin <marc.jauvin@gmail.com>
2021-03-17 11:26:23 -0400

    Revert "use new lazy Class Loader aliasing"
    This reverts commit d13613d4c6096f2631407987a11623fd548c17ac.

--- aliases.php
+++ aliases.php
@@ -1,7 +1,4 @@
 <?php
-
-use Winter\Storm\Support\ClassLoader;
-
 /**
  * To allow compatibility with plugins that extend the original RainLab.Blog plugin, this will alias those classes to
  * use the new Winter.Blog classes.
@@ -24,4 +21,8 @@ $aliases = [
     Winter\Blog\Models\PostExport::class          => 'RainLab\Blog\Models\PostExport',
 ];
 
-app(ClassLoader::class)->addAliases($aliases);
+foreach ($aliases as $original => $alias) {
+    if (!class_exists($alias)) {
+        class_alias($original, $alias);
+    }
+}
