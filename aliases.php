1e60ea012b1fa8a797bb88db1af72c51ac0c1d20
Marc Jauvin <marc.jauvin@gmail.com>
2021-03-17 12:04:02 -0400

    normalize class names

--- aliases.php
+++ aliases.php
@@ -7,21 +7,21 @@ use Winter\Storm\Support\ClassLoader;
  * use the new Winter.Blog classes.
  */
 $aliases = [
-    Winter\Blog\Plugin::class                     => 'RainLab\Blog\Plugin',
-    Winter\Blog\Components\Categories::class      => 'RainLab\Blog\Components\Categories',
-    Winter\Blog\Classes\TagProcessor::class       => 'RainLab\Blog\Classes\TagProcessor',
-    Winter\Blog\Components\Posts::class           => 'RainLab\Blog\Components\Posts',
-    Winter\Blog\Components\Post::class            => 'RainLab\Blog\Components\Post',
-    Winter\Blog\Components\RssFeed::class         => 'RainLab\Blog\Components\RssFeed',
-    Winter\Blog\Controllers\Categories::class     => 'RainLab\Blog\Controllers\Categories',
-    Winter\Blog\Controllers\Posts::class          => 'RainLab\Blog\Controllers\Posts',
-    Winter\Blog\FormWidgets\BlogMarkdown::class   => 'RainLab\Blog\FormWidgets\BlogMarkdown',
-    Winter\Blog\FormWidgets\MLBlogMarkdown::class => 'RainLab\Blog\FormWidgets\MLBlogMarkdown',
-    Winter\Blog\Models\Settings::class            => 'RainLab\Blog\Models\Settings',
-    Winter\Blog\Models\PostImport::class          => 'RainLab\Blog\Models\PostImport',
-    Winter\Blog\Models\Post::class                => 'RainLab\Blog\Models\Post',
-    Winter\Blog\Models\Category::class            => 'RainLab\Blog\Models\Category',
-    Winter\Blog\Models\PostExport::class          => 'RainLab\Blog\Models\PostExport',
+    Winter\Blog\Plugin::class                     => RainLab\Blog\Plugin::class,
+    Winter\Blog\Components\Categories::class      => RainLab\Blog\Components\Categories::class,
+    Winter\Blog\Classes\TagProcessor::class       => RainLab\Blog\Classes\TagProcessor::class,
+    Winter\Blog\Components\Posts::class           => RainLab\Blog\Components\Posts::class,
+    Winter\Blog\Components\Post::class            => RainLab\Blog\Components\Post::class,
+    Winter\Blog\Components\RssFeed::class         => RainLab\Blog\Components\RssFeed::class,
+    Winter\Blog\Controllers\Categories::class     => RainLab\Blog\Controllers\Categories::class,
+    Winter\Blog\Controllers\Posts::class          => RainLab\Blog\Controllers\Posts::class,
+    Winter\Blog\FormWidgets\BlogMarkdown::class   => RainLab\Blog\FormWidgets\BlogMarkdown::class,
+    Winter\Blog\FormWidgets\MLBlogMarkdown::class => RainLab\Blog\FormWidgets\MLBlogMarkdown::class,
+    Winter\Blog\Models\Settings::class            => RainLab\Blog\Models\Settings::class,
+    Winter\Blog\Models\PostImport::class          => RainLab\Blog\Models\PostImport::class,
+    Winter\Blog\Models\Post::class                => RainLab\Blog\Models\Post::class,
+    Winter\Blog\Models\Category::class            => RainLab\Blog\Models\Category::class,
+    Winter\Blog\Models\PostExport::class          => RainLab\Blog\Models\PostExport::class,
 ];
 
 app(ClassLoader::class)->addAliases($aliases);
