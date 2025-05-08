8f3599ff72ef754dd97e169ab8adf42cc176c0fb
Marc Jauvin <marc.jauvin@gmail.com>
2025-05-08 10:14:08 -0400

    remove unneeded partial

--- controllers/categories/_reorder_toolbar.php
+++
@@ -1,5 +0,0 @@
-<div data-control="toolbar">
-    <a href="<?= Backend::url('winter/blog/categories') ?>" class="btn btn-primary oc-icon-caret-left">
-        <?= e(trans('winter.blog::lang.category.return_to_categories')) ?>
-    </a>
-</div>
